e3c184c4165138cb4d63c4a39dca3f5a815a38bf
Weifc <2418060010@qq.com>
2020-09-08 21:47:21 +0800

    C++服务器框架04_日志系统04_formatter

--- sylar/sylar/log.h
+++ sylar/sylar/log.h
@@ -15,11 +15,20 @@ class LogEvent {
 public:
     typedef std::shared_ptr<LogEvent> ptr;
     LogEvent();
+
+    const char* getFile() const { return m_file; }
+    int32_t getLine() const { return m_line; }
+    uint32_t getElapse() const { return m_elapse; }
+    uint32_t getThreadId() const { return m_threadId; }
+    uint32_t getFiberId() const { return m_fiberId; }
+    uint64_t getTime() const { return m_time; }
+    const std::string& getContent() const { return m_content; }
+
 private:
     const char *m_file = nullptr; //文件名
     int32_t m_line = 0;           //行号
     uint32_t m_elapse = 0;        //程序启动开始到现在的毫秒数
-    uint32_t threadId = 0;        //线程id
+    uint32_t m_threadId = 0;        //线程id
     uint32_t m_fiberId = 0;       //协程id
     uint64_t m_time;              //时间戳
     std::string m_content;
@@ -30,21 +39,37 @@ private:
 class LogLevel {
 public:
     enum Level {
+        UNKNOW = 0,
         DEBUG = 1,
         INFO = 2,
         WARN = 3,
         ERROR = 4,
         FATAL = 5
     };
+
+    static cosnt char* ToString(LogLevel::Level);
 };
 
 //日志格式器
 class LogFormatter {
 public:
     typedef std::shared_ptr<LogFormatter> ptr;
+    LogFormatter(const std::string& pattern);
 
-    std::string format(LogEvent::ptr event);
+    //%t    %thread_id %m%n
+    std::string format(LogLevel::Level level, LogEvent::ptr event);
+public:
+    class FormatIten {
+        public:
+        typedef std::shared_ptr<FormatIten> ptr;
+        virtual ~FormatIten() {}
+        virtual void format(std::ostream& os, LogLevel::Level level, LogEvent::ptr event) = 0;
+    };
+
+    void init();
 private:
+    std::string m_pattern;
+    std::vector<FormatIten::ptr> m_items;
 };
 
 //日志输出地
